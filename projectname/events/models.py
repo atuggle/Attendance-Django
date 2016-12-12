a91d778e9c3df031336da8d9b89c759ff3c03b9e
allen.tuggle <allen.tuggle@excella.com>
2016-12-12 01:28:49 -0500

    Coded models and migrated db

--- projectname/events/models.py
+++ projectname/events/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
+from datetime import datetime
+from django.utils import timezone
 
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=70)
+    date_time = models.DateField('Event date time', default=timezone.now)
+    description = models.TextField()
+    enabled = models.BooleanField(default=True)
+
+    def __str__(self):           
+        return self.name
+
+class Person(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.full_name
+      
+class Attendance(models.Model):
+    person = models.ForeignKey(Person)
+    event = models.ForeignKey(Event)
+    
+    def __str__(self):
+        return "{0} is Attending the Event: {1}".format(self.person.full_name, self.event.name)
