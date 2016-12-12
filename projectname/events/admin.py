2768c7e8f746beb650c2eec0e55b6a7427322aab
allen.tuggle <allen.tuggle@excella.com>
2016-12-12 02:13:25 -0500

    Registered models to show in admin console

--- projectname/events/admin.py
+++ projectname/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    fields = ['enabled', 'date_time', 'name', 'description']
+
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.Person)
+admin.site.register(models.Attendance)
