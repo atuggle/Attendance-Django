0375a763fccb47a08c087bb6a43297d7e95d8cc3
Matt McHugh <mchughmk@gmail.com>
2016-12-13 12:11:29 -0500

    Adding list_display for admin columns

--- projectname/events/admin.py
+++ projectname/events/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 
 class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'date_time', 'enabled']
     fields = ['enabled', 'date_time', 'name', 'description']
 
 admin.site.register(models.Event, EventAdmin)
