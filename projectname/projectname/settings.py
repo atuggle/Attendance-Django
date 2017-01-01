5f3ae81c5914df56ac6b3d4c3e5edce6a6c1333f
allen.tuggle <allen.tuggle@excella.com>
2017-01-01 17:34:01 -0500

    Working on registration with registration-redux

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events.apps.EventsConfig',
@@ -121,9 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
 REGISTRATION_OPEN = True        # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
 LOGIN_REDIRECT_URL = '/events/' # The page you want users to arrive at after they successful log in
-LOGIN_URL = '/login/'  # The page users are directed to if they are not logged in, and are trying to access pages requiring authentication
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in, and are trying to access pages requiring authentication
--- projectname/projectname/urls.py
+++ projectname/projectname/urls.py
@@ -17,13 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+from registration.backends.simple.views import RegistrationView
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^events/', include('events.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^$', RedirectView.as_view(url='/events/'))
 ]
-
