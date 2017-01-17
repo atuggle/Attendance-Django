239fbae0fe4509da48be5427e8a355874620f9aa
allen.tuggle <allen.tuggle@excella.com>
2017-01-17 00:11:31 -0500

    Added crispy_formms

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events.apps.EventsConfig',
+    'crispy_forms',
     'registration'
 ]
 
@@ -122,8 +123,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#Crispy FORM TAGs SETTINGS
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+#Registration-redux
 REGISTRATION_OPEN = True        # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
 LOGIN_REDIRECT_URL = '/events/' # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in, and are trying to access pages requiring authentication
+SITE_ID = 1
